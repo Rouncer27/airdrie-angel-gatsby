cf897fe889939b1016c1b829b6515ef417ca996d
Trevor Rounce <trevorrounce@Trevors-iMac.hitronhub.home>
2019-02-16 21:31:57 -0700

    added the plants to the our mission page

--- src/components/sections/OurMission/Helped.js
+++ src/components/sections/OurMission/Helped.js
@@ -4,6 +4,9 @@ import styled from "styled-components";
 import { StandardWrapper } from "../../styles/Commons/Wrappers";
 import { StandardParagraph } from "../../styles/Commons/Paragraphs";
 import Trees from "../SceneParts/Trees";
+import PlantOne from "../SceneParts/PlantOne";
+import PlantTwo from "../SceneParts/PlantTwo";
+import PlantThree from "../SceneParts/PlantThree";
 
 const StyledHelped = styled.div`
   position: relative;
@@ -37,6 +40,66 @@ const HelpedIntro = styled.div`
   }
 `;
 
+const QouteSection = styled.div`
+  width: 100%;
+  margin: 0 auto;
+  padding: 5rem 2rem;
+
+  .helped-plant-one {
+    bottom: 3rem;
+    left: auto;
+    right: 2rem;
+    opacity: 0;
+
+    @media (min-width: ${props => props.theme.bpTablet}) {
+      opacity: 1;
+    }
+  }
+
+  .helped-plant-two {
+    bottom: 3rem;
+    right: auto;
+    left: 3rem;
+    opacity: 0;
+
+    @media (min-width: ${props => props.theme.bpTablet}) {
+      opacity: 1;
+    }
+  }
+
+  .helped-plant-three {
+    bottom: 3rem;
+    right: auto;
+    left: 3rem;
+    opacity: 0;
+
+    @media (min-width: ${props => props.theme.bpTablet}) {
+      opacity: 1;
+    }
+  }
+
+  .quoteItem {
+    position: relative;
+    margin-bottom: 2rem;
+    padding: 4rem 12rem;
+    text-align: center;
+
+    &__top {
+      p {
+        margin: 0;
+        font-size: 3rem;
+        font-family: ${props => props.theme.fontSec};
+      }
+    }
+
+    &__bot {
+      p {
+        margin: 0;
+      }
+    }
+  }
+`;
+
 class Helped extends Component {
   render() {
     return (
@@ -46,18 +109,31 @@ class Helped extends Component {
         />
         <Trees location="helped-trees" />
         <StandardWrapper>
-          <div>
+          <QouteSection>
             {this.props.data.helpedPeople.map((people, index) => {
+              let plantIcon = false;
+              if ((index + 1) % 2 === 0) {
+                plantIcon = <PlantOne location="helped-plant-one" />;
+              } else if ((index + 1) % 3 === 0) {
+                plantIcon = <PlantTwo location="helped-plant-two" />;
+              } else {
+                plantIcon = <PlantThree location="helped-plant-three" />;
+              }
               return (
-                <div key={index}>
-                  <div dangerouslySetInnerHTML={{ __html: people.big_quote }} />
+                <div key={index} className="quoteItem">
                   <div
+                    className="quoteItem__top"
+                    dangerouslySetInnerHTML={{ __html: people.big_quote }}
+                  />
+                  <div
+                    className="quoteItem__bot"
                     dangerouslySetInnerHTML={{ __html: people.small_quote }}
                   />
+                  {plantIcon}
                 </div>
               );
             })}
-          </div>
+          </QouteSection>
         </StandardWrapper>
       </StyledHelped>
     );
